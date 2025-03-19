eeea3f1e83917aa625a96f669b898469b7790372
Koukyosyumei <koukyosyumei@hotmail.com>
2025-03-19 12:59:59 -0400

    update

--- Chapter2/Equiv.lean
+++ Chapter2/Equiv.lean
@@ -1,4 +1,5 @@
 import Mathlib.Data.Nat.Basic
+import Mathlib.Tactic
 
 -- A `state` can be modeled as a function mapping variable names
 -- to values (e.g., natural numbers in this tutorial)
@@ -25,6 +26,10 @@ inductive ceval : com -> state -> state -> Prop
     ceval com.skip st st
 | E_Assign : ∀ (st : state) (x : String) (n : ℕ),
     ceval (com.assign x n) st (set st x n)
+| E_Seq : ∀ (c1 c2 : com) (st st' st'' : state),
+    ceval c1 st st' →
+    ceval c2 st' st'' →
+    ceval (com.seq c1 c2) st st''
 | E_IfTrue : ∀ (st st' : state) (b : Bool) (c1 c2 : com),
     b = true → ceval c1 st st' → ceval (com.if_ b c1 c2) st st'
 | E_IfFalse : ∀ (st st' : state) (b : Bool) (c1 c2 : com),
@@ -39,3 +44,23 @@ inductive ceval : com -> state -> state -> Prop
 theorem skip_preserves_state (st : state) :
   ceval com.skip st st := by
   apply ceval.E_Skip
+
+-- Proving a command equivalence
+
+-- ∀c, (skip; c) is equivalent to c
+theorem skip_left (c : com) (st st' : state) :
+  ceval (com.seq com.skip c) st st' ↔ ceval c st st' := by
+  -- breaking the statement into two directions
+  constructor
+  -- forward (→)
+  . intro h
+    cases h
+    case E_Seq st'' h_skip h_c =>
+        cases h_skip
+        case E_Skip =>
+            exact h_c
+  -- backward (←)
+  . intro h
+    apply ceval.E_Seq
+    . apply ceval.E_Skip
+    . exact
